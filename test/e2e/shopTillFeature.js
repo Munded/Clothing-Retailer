2bca258703d7d76a159c05867fea4dc4ed8a4cd7
Edmund O'Brien <edobrien@Edmunds-MacBook-Air.local>
2015-06-15 19:04:51 +0100

    can remove and calculate items

--- test/e2e/shopTillFeature.js
+++ test/e2e/shopTillFeature.js
@@ -11,16 +11,36 @@ describe('ShopTillYouDrop homepage', function() {
   	expect(element(by.id('wf')).getText()).toContain("Women’s Footwear");
  	});
 
-  it('displays an empty cart when no items added', function() {
-  	expect(element(by.id('cart')).getText()).toContain('Your cart is empty');
-  });
 
   describe('it can add items to cart', function() {
+
+	 	it('displays an empty cart when no items added', function() {
+			expect(element(by.id('cart')).getText()).toContain('Your cart is empty');
+	  });
+
 		it('can add an item to the shopping basket', function() {
-			element.all(by.repeater("item in ctrl.inventory | filter: { category:'Men’s Formalwear'}")).
-		  get(1).
-		  element(by.linkText('Add to Cart')).click()  
-		  expect(element(by.id('cart')).getText()).toContain('175.50');
+			element.all(by.repeater("item in ctrl.inventory | filter: { category:'Women’s Footwear'}")).
+			get(0).
+			element(by.linkText('Add to Cart')).click()  
+		  expect(element(by.id('cart')).getText()).toContain('99');
 		 });
+
+		it('can remove an item from the basket', function() {
+			element.all(by.repeater("item in ctrl.inventory | filter: { category:'Women’s Footwear'}")).
+			get(0).
+			element(by.linkText('Add to Cart')).click()
+			element(by.css('.glyphicon-remove')).click()
+			expect(element(by.id('cart')).getText()).toContain('Your cart is empty');
+		});
+
+		it('can calculate total', function() {
+			element.all(by.repeater("item in ctrl.inventory | filter: { category:'Women’s Footwear'}")).
+			get(0).
+			element(by.linkText('Add to Cart')).click()
+			element.all(by.repeater("item in ctrl.inventory | filter: { category:'Women’s Footwear'}")).
+			get(1).
+			element(by.linkText('Add to Cart')).click()
+			expect(element(by.id('cart')).getText()).toContain('141');
+		})
 	});
 });
